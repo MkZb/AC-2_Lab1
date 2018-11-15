c74ec6374e076bf1ae55720af541f40ddc72950f
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:43:25 +0200

    Cherry-picked commit + added own changes + commited patches in separate folder

--- calculator.cpp
+++ calculator.cpp
@@ -7,5 +7,14 @@ int Calculator::Add (double a, double b)
 
 int Calculator::Sub (double a, double b)
 {
-    return Add (a, -b);
+    return Add (-a, b);
 }
+
+int Calculator::Mul (double a, double b)
+{
+    return a * b + 0.5 - 0.1;
+}
+
+//The changes
+//Made as a task
+//Nothing special
